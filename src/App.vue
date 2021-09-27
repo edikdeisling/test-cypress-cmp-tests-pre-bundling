bf68b223e0c401575c9d18b2daf3e859153827dd
Deisling Eduard <edikdeisling@gmail.com>
2021-09-27 16:14:49 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div data-test="app">{{ hello }}</div>
+</template>
+
+<script lang="ts" setup>
+  import { computed } from 'vue';
+
+  const hello = computed(() => 'Hello world')
+</script>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'element-plus/es/components/button/style/css.js';
+
+createApp(App).mount('#app');
